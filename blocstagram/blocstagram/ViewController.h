180e97fa87032e9b50ba90d85c0af88a06aee83f
risachi <risachi@gmail.com>
2016-03-26 16:29:47 -0700

    first commit

---
+++ blocstagram/blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blocstagram
+//
+//  Created by Lisa on 3/26/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ blocstagram/blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  blocstagram
+//
+//  Created by Lisa on 3/26/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
