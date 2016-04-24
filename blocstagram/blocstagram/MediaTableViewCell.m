77d9399d60e9d84e19040a4fa1ef36b01f0fb2cd
risachi <risachi@gmail.com>
2016-04-23 21:29:57 -0700

    I think I can... I think I can..

--- blocstagram/blocstagram/MediaTableViewCell.m
+++ blocstagram/blocstagram/MediaTableViewCell.m
@@ -199,11 +199,11 @@ static NSParagraphStyle *paragraphStyle; //lets us st properties like line spaci
     self.usernameAndCaptionLabelHeightConstraint.constant = usernameLabelSize.height == 0 ? 0 : usernameLabelSize.height + 20;
     self.commentLabelHeightConstraint.constant = commentLabelSize.height == 0 ? 0: commentLabelSize.height + 20;
     
-    //if self.mediaItem.image.size.width is zero, the calculatiou will cause a divide-by-zero, which is illegal and will cause a crash
+    //if self.mediaItem.image.size.width is zero, the calculation will cause a divide-by-zero, which is illegal and will cause a crash
     if (self.mediaItem.image.size.width > 0 && CGRectGetWidth(self.contentView.bounds) > 0) {
         self.imageHeightConstraint.constant = self.mediaItem.image.size.height / self.mediaItem.image.size.width * CGRectGetWidth(self.contentView.bounds);
     } else {
-        self.imageHeightConstraint.constant = 0;
+        self.imageHeightConstraint.constant = 10;
     }
 
     // hide the line between cells
